bacc20f2d0b272a637d4e92be6e6b9bdc5eea71f
Farid <101619655+faridibirov@users.noreply.github.com>
2026-01-16 15:21:40 +0400

    endpoints in coupon service, bootswatch theme and bootstrap icons, coupon controller, api call in action, coupon index and create view, create/delete coupon in action, toatr notifications

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -9,7 +9,7 @@ using static Azure.Core.HttpHeader;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/coupon")]
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
@@ -117,6 +117,7 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpDelete]
+        [Route("{id:int}")]
         public ResponseDto Delete(int id)
         {
             try
