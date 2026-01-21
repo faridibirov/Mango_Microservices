d4f782f7e63489220a5afc0c24ccea554ef1f0e7
Farid <101619655+faridibirov@users.noreply.github.com>
2026-01-21 18:00:12 +0400

    login/register ui, role, register/login in action, token provider services, sign in user in .net identity, logut in action, adding roles in token

--- Mango.Web/Models/LoginRequestDTO.cs
+++ Mango.Web/Models/LoginRequestDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace Mango.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models;
 
 public class LoginRequestDTO
 {
+    [Required]
     public string UserName { get; set; }
+    [Required]
     public string Password { get; set; }
 }
