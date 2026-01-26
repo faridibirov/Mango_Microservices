1bf5adf588021f951c141e2ad2251a6c8845244d
Farid <101619655+faridibirov@users.noreply.github.com>
2026-01-26 16:57:45 +0400

    Shopping Cart

---
+++ Mango.Services.ShoppingCartAPI/Service/CouponService.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Service.IService;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using Newtonsoft.Json;
+
+namespace Mango.Services.ShoppingCartAPI.Service;
+
+public class CouponService : ICouponService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CouponService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<CouponDTO> GetCoupon(string couponCode)
+    {
+        var client = _httpClientFactory.CreateClient("Coupon");
+        var response = await client.GetAsync($"/api/coupon/GetByCode/{couponCode}");
+        var apiContent = await response.Content.ReadAsStringAsync();
+        var resp = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+
+        if (resp.IsSuccess)
+        {
+            return JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(resp.Result));
+        }
+        return new CouponDTO();
+    }
+}
