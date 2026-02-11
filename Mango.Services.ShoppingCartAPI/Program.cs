6fc0b5b08f5b6beb99031a75fd2a4143b10c75a4
Farid Dibirov <farid962@gmail.com>
2026-02-11 21:16:11 +0400

    small bugs fixed

--- Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -73,8 +73,11 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CART API");
-    c.RoutePrefix = string.Empty;
+    if (!app.Environment.IsDevelopment())
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cart API");
+        c.RoutePrefix = string.Empty;
+    }
 });
 
 app.UseHttpsRedirection();
